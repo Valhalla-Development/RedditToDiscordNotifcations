8d21551e57b671e02dd82881b73291de7bd46d25
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-09-28 19:49:30 +0100

    📝 (index.ts): refactor checkRequiredEnvVars function to use arrow function syntax and improve readability 📝 (index.ts): refactor setupFeed function to use arrow function syntax and improve error handling 📝 (index.ts): refactor processWebhook function to use arrow function syntax and improve error handling 📝 (index.ts): refactor extractTextFromDescription function to use arrow function syntax and improve code structure

--- src/index.ts
+++ src/index.ts
@@ -3,34 +3,22 @@ import RssFeedEmitter from 'rss-feed-emitter';
 import 'dotenv/config';
 import { decode } from 'html-entities';
 
-let thumbnail: string;
+/** List of required environment variables */
+const requiredEnvVars = [
+    'WebhookUrl', 'WebhookUsername', 'WebhookAvatar',
+    'EmbedAuthorImageUrl', 'RssUrl', 'RssName',
+];
 
 /**
- * Checks if the required environment variables are defined.
- * Throws an error with a list of missing variables if any are not set.
- * @function checkRequiredEnvVars
- * @throws An error if any of the required environment variables are missing.
+ * Checks if all required environment variables are set.
+ * @throws Error If any required variable is missing.
  */
-function checkRequiredEnvVars(): void {
-    // Array of required environment variable names
-    const requiredVars = [
-        'WebhookUrl',
-        'WebhookUsername',
-        'WebhookAvatar',
-        'EmbedAuthorImageUrl',
-        'RssUrl',
-        'RssName',
-    ];
+const checkRequiredEnvVars = () => {
+    const missingVars = requiredEnvVars.filter((varName) => !process.env[varName]);
+    if (missingVars.length) throw new Error(`Missing environment variables: ${missingVars.join(', ')}`);
+};
 
-    // Filtering out the missing environment variables
-    const missingVars = requiredVars.filter((varName) => !process.env[varName]);
-
-    // If any required variables are missing, throw an error
-    if (missingVars.length > 0) {
-        throw new Error(`Missing required environment variables: ${missingVars.join(', ')}`);
-    }
-}
-
+// Check environment variables and exit if any are missing
 try {
     checkRequiredEnvVars();
 } catch (error) {
@@ -38,160 +26,105 @@ try {
     process.exit(1);
 }
 
-const hook = new Webhook(process.env.WebhookUrl as string);
-hook.setUsername(process.env.WebhookUsername as string);
-hook.setAvatar(process.env.WebhookAvatar as string);
+// Initialize Discord webhook
+const hook = new Webhook(process.env.WebhookUrl!)
+    .setUsername(process.env.WebhookUsername!)
+    .setAvatar(process.env.WebhookAvatar!);
 
 /**
- * Sets up and monitors an RSS feed using the RssFeedEmitter library.
+ * Sets up and monitors an RSS feed.
  * @async
- * @function setupFeed
- * @returns A promise that resolves when the feed is successfully set up.
  */
-async function setupFeed(): Promise<void> {
+const setupFeed = async () => {
     try {
         const feeder = new RssFeedEmitter({ skipFirstLoad: true });
-
         feeder.on('error', console.error);
-
-        // Adding the RSS feed configuration to the feeder
         feeder.add({
-            url: process.env.RssUrl as string,
+            url: process.env.RssUrl!,
             refresh: 60000,
-            eventName: process.env.RssName as string,
+            eventName: process.env.RssName!,
         });
 
-        // Event listener for the 'AirReps' event triggered by the feeder
         feeder.on('AirReps', async (res) => {
-            // Check if post was published over 12 hours ago and return if so
-            if ((new Date().getTime() - new Date(res.pubDate).getTime()) / (1000 * 60 * 60) > 12) {
-                return;
-            }
-
+            // Ignore posts older than 12 hours
+            if ((Date.now() - new Date(res.pubDate).getTime()) / 3600000 > 12) return;
             await processWebhook(res);
         });
 
         console.log('RSS feed monitoring started...');
     } catch (error) {
-        // Handling setup errors and retrying after a delay
         console.error('Error setting up RSS feed:', error);
-        console.log('Retrying in 10 seconds...');
-        setTimeout(setupFeed, 10 * 1000);
+        setTimeout(setupFeed, 10000);
     }
-}
-
-await setupFeed();
+};
 
 /**
- * Processes the webhook response and sends a Discord webhook with the extracted information.
+ * Processes the RSS feed item and sends a Discord webhook.
  * @async
- * @function processWebhook
- * @param res - The webhook response containing information to be processed.
- * @returns A promise that resolves when the webhook processing is complete.
+ * @param res - The RSS feed item.
  */
-async function processWebhook(res: {
+const processWebhook = async (res: {
     description: string;
     title: string;
     link: string;
     author: string;
-    image: {
-        url?: string
-    }
-    guid: string
-}): Promise<void> {
-    // Extracting text from the description field
+    image: { url?: string };
+    guid: string;
+}) => {
     const extractedText = extractTextFromDescription(res);
-
     if (!extractedText) return;
 
-    // Building a Discord message embed
     const embed = new MessageBuilder()
         .setTitle(res.title)
         // @ts-expect-error: The creator of the package incorrectly defined this as setUrl instead of setURL in its type file
         .setURL(res.link)
         .setColor('#FF4500')
         .setDescription(extractedText.join('\n'))
-        .setFooter(`${res.author} | ${returnFormattedDate()}`, process.env.EmbedAuthorImageUrl as string);
+        .setFooter(`${res.author} | ${new Date().toLocaleString('en-US', {
+            month: 'long',
+            day: 'numeric',
+            year: 'numeric',
+            hour: 'numeric',
+            minute: '2-digit',
+            hour12: true,
+        })}`, process.env.EmbedAuthorImageUrl!);
 
-    if (thumbnail) {
-        embed.setThumbnail(thumbnail);
-        thumbnail = '';
+    if (res.image.url) {
+        embed.setThumbnail(res.image.url);
     }
 
     try {
-        // Sending the embed to the Discord webhook
         await hook.send(embed);
     } catch (error) {
-        // Handling errors that may occur while sending the webhook
         console.error('Error sending Discord webhook:', error);
     }
-}
+};
 
 /**
- * Extracts text content or image links from HTML description.
- *
- * This function uses regular expressions to match text content or image links within an HTML structure.
- * If text content is found, it is extracted, HTML tags are removed, and paragraphs are formatted with newlines.
- * If no text content is found, it checks for image links and returns them.
- *
- * @param res - The JSON response from the RSS feed.
- * @returns Extracted text content or image links, formatted accordingly.
+ * Extracts text content from the RSS feed item description.
+ * @param {Object} res - The RSS feed item.
+ * @returns {string[] | null} Array of an extracted text or null if no content.
  */
-function extractTextFromDescription(res: {
+const extractTextFromDescription = (res: {
     description: string;
-    title: string;
-    link: string;
-    author: string;
-    image: {
-        url?: string
-    }
-    guid: string
-}): string[] | null {
+    image: { url?: string };
+    guid: string;
+}): string[] | null => {
     const arr: string[] = [];
 
-    // Check for images in the post
-    if (Object.keys(res.image).length > 0) {
-        if (res.image.url) thumbnail = res.image.url;
-
+    // Add an image link if present
+    if (res.image.url) {
         arr.push(`[**Image**](https://www.reddit.com/gallery/${res.guid.replace(/^t\d_/, '')})`);
     }
 
-    // Regular expression to match the entire div and its content
-    const mdRegex = /<div class="md">([\s\S]*?)<\/div>/;
-
-    // Attempt to find a match for text content
-    const mdMatch = res.description.match(mdRegex);
-
+    // Extract and decode text content
+    const mdMatch = res.description.match(/<div class="md">([\s\S]*?)<\/div>/);
     if (mdMatch) {
-        // If there is a match for text content, extract text, remove HTML tags, and handle paragraphs
-        const str = mdMatch[1]
-            .replace(/<p>/g, '\n\n') // Replace paragraph tags with newlines
-            .replace(/<[^>]*>/g, '') // Remove all remaining HTML tags
-            .trim();
-
-        arr.push(decode(str));
+        arr.push(decode(mdMatch[1].replace(/<p>/g, '\n\n').replace(/<[^>]*>/g, '').trim()));
     }
 
-    // Return either the array, or null
     return arr.length ? arr : null;
-}
+};
 
-/**
- * Returns the current date and time formatted as a string.
- * @function returnFormattedDate
- * @returns The formatted date string.
- */
-function returnFormattedDate(): string {
-    // Getting the current date and time
-    const today = new Date();
-
-    // Formatting the date string using the 'toLocaleString' method
-    return today.toLocaleString('en-US', {
-        month: 'long',
-        day: 'numeric',
-        year: 'numeric',
-        hour: 'numeric',
-        minute: '2-digit',
-        hour12: true,
-    });
-}
+// Start the RSS feed monitoring
+await setupFeed();
